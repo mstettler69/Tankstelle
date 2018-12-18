fbe2d9cf4d925b2cff776458767e1c2d491e7152
Myles Stettler <smilinho@gmx.ch>
2018-12-18 14:35:27 +0100

    First commit

---
+++ Tankstelle/FormSaeule.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Tankstelle
+{
+    partial class FormSaeule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBleifrei95 = new System.Windows.Forms.Button();
+            this.btnBleifrei98 = new System.Windows.Forms.Button();
+            this.btnDiesel = new System.Windows.Forms.Button();
+            this.btnStrom = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnTankEnde = new System.Windows.Forms.Button();
+            this.lblLiterPreis = new System.Windows.Forms.Label();
+            this.lblGetankt = new System.Windows.Forms.Label();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBleifrei95
+            // 
+            this.btnBleifrei95.Location = new System.Drawing.Point(12, 12);
+            this.btnBleifrei95.Name = "btnBleifrei95";
+            this.btnBleifrei95.Size = new System.Drawing.Size(143, 197);
+            this.btnBleifrei95.TabIndex = 0;
+            this.btnBleifrei95.Text = "Bleifrei 95";
+            this.btnBleifrei95.UseVisualStyleBackColor = true;
+            // 
+            // btnBleifrei98
+            // 
+            this.btnBleifrei98.Location = new System.Drawing.Point(161, 12);
+            this.btnBleifrei98.Name = "btnBleifrei98";
+            this.btnBleifrei98.Size = new System.Drawing.Size(143, 197);
+            this.btnBleifrei98.TabIndex = 1;
+            this.btnBleifrei98.Text = "Bleifrei 98";
+            this.btnBleifrei98.UseVisualStyleBackColor = true;
+            // 
+            // btnDiesel
+            // 
+            this.btnDiesel.Location = new System.Drawing.Point(310, 12);
+            this.btnDiesel.Name = "btnDiesel";
+            this.btnDiesel.Size = new System.Drawing.Size(143, 197);
+            this.btnDiesel.TabIndex = 2;
+            this.btnDiesel.Text = "Diesel";
+            this.btnDiesel.UseVisualStyleBackColor = true;
+            // 
+            // btnStrom
+            // 
+            this.btnStrom.Location = new System.Drawing.Point(459, 12);
+            this.btnStrom.Name = "btnStrom";
+            this.btnStrom.Size = new System.Drawing.Size(143, 197);
+            this.btnStrom.TabIndex = 3;
+            this.btnStrom.Text = "Strom";
+            this.btnStrom.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Literpreis";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Getankt";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 354);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Saldo";
+            // 
+            // btnTankEnde
+            // 
+            this.btnTankEnde.Enabled = false;
+            this.btnTankEnde.Location = new System.Drawing.Point(481, 393);
+            this.btnTankEnde.Name = "btnTankEnde";
+            this.btnTankEnde.Size = new System.Drawing.Size(121, 35);
+            this.btnTankEnde.TabIndex = 10;
+            this.btnTankEnde.Text = "Tankvorgang beenden";
+            this.btnTankEnde.UseVisualStyleBackColor = true;
+            // 
+            // lblLiterPreis
+            // 
+            this.lblLiterPreis.AutoSize = true;
+            this.lblLiterPreis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLiterPreis.Location = new System.Drawing.Point(216, 263);
+            this.lblLiterPreis.Name = "lblLiterPreis";
+            this.lblLiterPreis.Size = new System.Drawing.Size(51, 20);
+            this.lblLiterPreis.TabIndex = 11;
+            this.lblLiterPreis.Text = "label4";
+            // 
+            // lblGetankt
+            // 
+            this.lblGetankt.AutoSize = true;
+            this.lblGetankt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGetankt.Location = new System.Drawing.Point(216, 312);
+            this.lblGetankt.Name = "lblGetankt";
+            this.lblGetankt.Size = new System.Drawing.Size(51, 20);
+            this.lblGetankt.TabIndex = 12;
+            this.lblGetankt.Text = "label5";
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.Location = new System.Drawing.Point(216, 363);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(51, 20);
+            this.lblSaldo.TabIndex = 13;
+            this.lblSaldo.Text = "label6";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 440);
+            this.Controls.Add(this.lblSaldo);
+            this.Controls.Add(this.lblGetankt);
+            this.Controls.Add(this.lblLiterPreis);
+            this.Controls.Add(this.btnTankEnde);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStrom);
+            this.Controls.Add(this.btnDiesel);
+            this.Controls.Add(this.btnBleifrei98);
+            this.Controls.Add(this.btnBleifrei95);
+            this.Name = "Form2";
+            this.Text = "Zapfsäule";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBleifrei95;
+        private System.Windows.Forms.Button btnBleifrei98;
+        private System.Windows.Forms.Button btnDiesel;
+        private System.Windows.Forms.Button btnStrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnTankEnde;
+        private System.Windows.Forms.Label lblLiterPreis;
+        private System.Windows.Forms.Label lblGetankt;
+        private System.Windows.Forms.Label lblSaldo;
+    }
+}
---
+++ Tankstelle/Zapfsaule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tankstelle
+{
+    class Zapfsaule
+    {
+
+        public int SauleNr;
+        public string[] Kraftstoffarten;
+        public int getankt;
+        public bool aktiv = false;
+        public bool gesperrt = false;
+
+    }
+}
